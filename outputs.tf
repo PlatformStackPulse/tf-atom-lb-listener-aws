1ca9550ef886a813bfc8bcedc00ef37e47e0ebe6
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-26 23:16:01 +0100

    feat: implement tf-atom-lb-listener-aws module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
 output "enabled" {
-  description = "Whether the module is enabled."
+  description = "Whether the module is enabled"
   value       = local.enabled
+}
+
+output "id" {
+  description = "ID of the listener"
+  value       = try(aws_lb_listener.this[0].id, null)
+}
+
+output "arn" {
+  description = "ARN of the listener"
+  value       = try(aws_lb_listener.this[0].arn, null)
 }
