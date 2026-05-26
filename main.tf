e318fe23ce76451e574d933f4fc2cd5132cca340
PE Stack Pulse <harry.sarshogh@gmail.com>
2026-05-26 23:25:59 +0100

    feat: implement tf-atom-lb-listener-aws module (#1)

---
+++ main.tf
@@ -0,0 +1,34 @@
+resource "aws_lb_listener" "this" {
+  count = module.this.enabled ? 1 : 0
+
+  load_balancer_arn = var.load_balancer_arn
+  port              = var.port
+  protocol          = var.protocol
+  ssl_policy        = var.protocol == "HTTPS" || var.protocol == "TLS" ? var.ssl_policy : null
+  certificate_arn   = var.protocol == "HTTPS" || var.protocol == "TLS" ? var.certificate_arn : null
+
+  default_action {
+    type             = var.default_action_type
+    target_group_arn = var.default_action_type == "forward" ? var.target_group_arn : null
+
+    dynamic "redirect" {
+      for_each = var.default_action_type == "redirect" ? [1] : []
+      content {
+        port        = var.redirect_port
+        protocol    = var.redirect_protocol
+        status_code = var.redirect_status_code
+      }
+    }
+
+    dynamic "fixed_response" {
+      for_each = var.default_action_type == "fixed-response" ? [1] : []
+      content {
+        content_type = var.fixed_response_content_type
+        message_body = var.fixed_response_message_body
+        status_code  = var.fixed_response_status_code
+      }
+    }
+  }
+
+  tags = module.this.tags
+}
